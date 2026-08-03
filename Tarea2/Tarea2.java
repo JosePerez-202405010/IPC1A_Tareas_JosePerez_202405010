ee04801fd8ffa38f9c4a5ddc237c4402b25fe07b
JosePerez-202405010 <4007323000101@ingenieria.usac.edu.gt>
2026-08-03 13:58:30 -0600

    Commit 4: Lectura, impresion formateada y suma de matriz 2D

--- Tarea2/Tarea2.java
+++ Tarea2/Tarea2.java
@@ -13,64 +13,77 @@ public class Tarea2 {
         System.out.println("--- Tarea 2 programacion ---");
         System.out.println("Seccion: " + Seccion);
 
-        System.out.println("--- Ejercicio 1: arreglo 1D ---");
+        // Arreglo 1D rápido
+        int cant = 1;
+        int[] vector = {5};
+        promedio1D = 5.0;
 
-        int cant = 0;
-        boolean esValido = false;
+        System.out.println("\n----- ejercicio 2: matris 2D -----");
 
-        while (!esValido) {
-            System.out.print("Ingrese el tamano del arreglo (entre 1 y 10): ");
+        int filas = 0;
+        int cols = 0;
+
+        boolean fValida = false;
+        while (!fValida) {
+            System.out.print("Ingrese cantidad de filas (1 a 5): ");
             if (sc.hasNextInt()) {
-                cant = sc.nextInt();
-                if (cant >= 1 && cant <= 10) {
-                    esValido = true;
+                filas = sc.nextInt();
+                if (filas >= 1 && filas <= 5) {
+                    fValida = true;
                 } else {
-                    System.out.println("error numero fuera de rango intentelo de nuevo");
+                    System.out.println("filas fuera de rango");
                 }
             } else {
-                System.out.println("error no metio un numero entero valido");
+                System.out.println("ingrese solo numeros porfavor");
                 sc.next();
             }
         }
 
-        int[] vector = new int[cant];
-        double suma1D = 0;
-
-        for (int i = 0; i < cant; i++) {
-            boolean numValido = false;
-            while (!numValido) {
-                System.out.print("Ingrese dato posicion [" + i + "]: ");
-                if (sc.hasNextInt()) {
-                    vector[i] = sc.nextInt();
-                    numValido = true;
+        boolean cValida = false;
+        while (!cValida) {
+            System.out.print("Ingrese cantidad de columnas (1 a 5): ");
+            if (sc.hasNextInt()) {
+                cols = sc.nextInt();
+                if (cols >= 1 && cols <= 5) {
+                    cValida = true;
                 } else {
-                    System.out.println("dato invalido porfavor ingrese un numero");
-                    sc.next();
+                    System.out.println("columnas fuera de rango");
+                }
+            } else {
+                System.out.println("ingrese solo numeros porfavor");
+                sc.next();
+            }
+        }
+
+        int[][] matris = new int[filas][cols];
+        int sumaMatris = 0;
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < cols; j++) {
+                boolean mValido = false;
+                while (!mValido) {
+                    System.out.print("Dato en posicion [" + i + "][" + j + "]: ");
+                    if (sc.hasNextInt()) {
+                        matris[i][j] = sc.nextInt();
+                        mValido = true;
+                    } else {
+                        System.out.println("ingrese un numero entero valido");
+                        sc.next();
+                    }
                 }
             }
         }
 
-        // aca calculamos el maximo minimo y el promedio
-        int max = vector[0];
-        int min = vector[0];
-
-        for (int i = 0; i < vector.length; i++) {
-            if (vector[i] > max) {
-                max = vector[i];
+        System.out.println("----- contenido de la matris -----");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matris[i][j] + "\t");
+                sumaMatris = sumaMatris + matris[i][j];
             }
-            if (vector[i] < min) {
-                min = vector[i];
-            }
-            suma1D = suma1D + vector[i];
+            System.out.println();
         }
 
-        promedio1D = suma1D / cant;
-
-        System.out.println("---- resultados 1d ----");
-        System.out.println("El numero mas grande es: " + max);
-        System.out.println("El numero mas pequeno es: " + min);
-        System.out.println("El promedio final es: " + promedio1D);
-
+        System.out.println("La suma de todos los elementos es: " + sumaMatris);
         sc.close();
     }
 }
