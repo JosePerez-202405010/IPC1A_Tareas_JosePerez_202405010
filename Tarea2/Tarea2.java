6df8e77f00cfc8280c27a72537656498f2850d6b
JosePerez-202405010 <4007323000101@ingenieria.usac.edu.gt>
2026-08-03 13:56:35 -0600

    Commit 1: Estructura inicial del proyecto y declaracion de variables primitivas

---
+++ Tarea2/Tarea2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Tarea2 {
+
+    public static void main(String[] args) {
+        // aca declaro los 4 tipos de datos primitivos 
+        int ejecucion = 0;
+        double promedio1D = 0.0;
+        boolean seguir = true;
+        char Seccion = 'A';
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Tarea 2 programacion ---");
+        System.out.println("Seccion: " + Seccion);
+
+        System.out.println("Estructura base del proyecto inicializada.");
+        sc.close(); 
+    }
+}
